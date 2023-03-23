ce4c119621a759b03ea6cc3eb3c11331ba6cd213
andreev-aleksey <94741962+andreev-aleksey@users.noreply.github.com>
2023-03-23 16:22:25 +0300

    Update IBank_part1_3.py

--- Module8/practice/IBank/IBank_part1_3.py
+++ Module8/practice/IBank/IBank_part1_3.py
@@ -1,5 +1,45 @@
 class Account:
     # TODO-0: скопируйте реализацию из предыдущего решения
+    def __init__(self, name: str, passport: str, phone_number: str, start_balance: int = 0):
+        self.name = name
+        self.passport = passport
+        self.phone_number = phone_number
+        # self.balance = start_balance
+        self.__balance = start_balance  # TODO: Закрываем прямой доступ к балансу
+
+    def full_info(self) -> str:
+        """
+        Полная информация о счете в формате: "Иван баланс: 100 руб. паспорт: 3200 123456 т.+7-900-200-02-03"
+        """
+        return f"{self.name} баланс: {self.__balance} руб. паспорт: {self.passport} т.{self.phone_number}"
+
+    def __repr__(self) -> str:
+        """
+        :return: Информацию о счете в виде строки в формате "Иван баланс: 100 руб."
+        """
+        return f"{self.name} баланс: {self.__balance} руб."
+
+    @property
+    def balance(self) -> int:
+        return self.__balance
+
+    def deposit(self, amount: int) -> None:
+        """
+        Внесение суммы на текущий счет
+        :param amount: сумма
+        """
+        self.__balance += amount 
+
+    def withdraw(self, amount: int) -> None:
+        """
+        Снятие суммы с текущего счета
+        :param amount: сумма
+        """
+        new_balance = self.__balance - amount
+        if new_balance >= 0:
+            self.__balance = new_balance
+        else:
+            raise ValueError("Недостаточно денег на счете")
 
     # TODO-1: напишите реализацию метода transfer()
     def transfer(self, target_account: 'Account', amount: int) -> None:
@@ -9,7 +49,12 @@ class Account:
         :param amount: сумма перевода
         :return:
         """
-        pass
+        if self.balance >= amount:
+            self.withdraw(amount)
+            target_account.deposit(amount)
+        else:
+            raise ValueError("Недостаточно денег на счете для перевода")
+
 
 
 account1 = Account("Иван", "3230 634563", "+7-900-765-12-34", 1000)
